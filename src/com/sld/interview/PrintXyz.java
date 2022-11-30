831e2a2ac3d3284ae8a4e3de7e28d78210517549
shaolida <s-ld@foxmail.com>
2022-12-01 01:35:39 +0800

    多线程打印xyz 方法4 方法5

--- src/com/sld/interview/PrintXyz.java
+++ src/com/sld/interview/PrintXyz.java
@@ -1,7 +1,11 @@
 package com.sld.interview;
 
 import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.Semaphore;
 import java.util.concurrent.atomic.AtomicInteger;
+import java.util.concurrent.locks.Condition;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
 
 /**
  * 三个线程轮流打印xyz
@@ -10,11 +14,11 @@ import java.util.concurrent.atomic.AtomicInteger;
 public class PrintXyz {
 
     public static void main(String[] args) {
-        printXyzIn3Threads3();
+        printXyzIn3Threads5();
     }
 
     /**
-     * 方法1
+     * 方法1：AtomicInteger
      */
     public static void printXyzIn3Threads1() {
 
@@ -65,7 +69,7 @@ public class PrintXyz {
 
     static volatile String s = "x";
     /**
-     * 方法2
+     * 方法2：synchronized
      */
     public static void printXyzIn3Threads2() {
 
@@ -177,5 +181,139 @@ public class PrintXyz {
         thread2.start();
         thread3.start();
         cd1.countDown();
+    }
+
+    private static Lock lock = new ReentrantLock();
+    private static Condition A = lock.newCondition();
+    private static Condition B = lock.newCondition();
+    private static Condition C = lock.newCondition();
+    private static int count = 0;
+    /**
+     * 方法4：ReentrantLock和Condition
+     */
+    public static void printXyzIn3Threads4() {
+        Thread thread1 = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    lock.lock();
+                    for (int i = 0; i < 10; i++) {
+                        while (count % 3 != 0) { // 注意这里是不等于，也就是在count除以3余数为0前一致处于阻塞
+                            A.await(); // A释放lock锁
+                        }
+                        System.out.println("x");
+                        count++;
+                        B.signal(); // A执行完唤醒B线程
+                    }
+                } catch (Exception e) {
+                    e.printStackTrace();
+                } finally {
+                    lock.unlock();
+                }
+            }
+        });
+
+        Thread thread2 = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    lock.lock();
+                    for (int i = 0; i < 10; i++) {
+                        while (count % 3 != 1) {
+                            B.await();
+                        }
+                        System.out.println("y");
+                        count++;
+                        C.signal();
+                    }
+                } catch (Exception e) {
+                    e.printStackTrace();
+                } finally {
+                    lock.unlock();
+                }
+            }
+        });
+
+        Thread thread3 = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    lock.lock();
+                    for (int i = 0; i < 10; i++) {
+                        while (count % 3 != 2) {
+                            C.await();
+                        }
+                        System.out.println("z");
+                        count++;
+                        A.signal();
+                    }
+                } catch (Exception e) {
+                    e.printStackTrace();
+                } finally {
+                    lock.unlock();
+                }
+            }
+        });
+
+        thread1.start();
+        thread2.start();
+        thread3.start();
+    }
+
+    private static Semaphore X = new Semaphore(1);
+    private static Semaphore Y = new Semaphore(0);
+    private static Semaphore Z = new Semaphore(0);
+    /**
+     * 方法5：Semaphore
+     */
+    public static void printXyzIn3Threads5() {
+        Thread thread1 = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    for (int i = 0; i < 10; i++) {
+                        X.acquire(); // X获取信号执行，X信号量减1，当X为0时将无法继续获取该信号量
+                        System.out.println("x");
+                        Y.release(); // Y释放信号，Y信号量加1（初始化为0），此时可以获取B信号量
+                    }
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+
+        Thread thread2 = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    for (int i = 0; i < 10; i++) {
+                        Y.acquire();
+                        System.out.println("Y");
+                        Z.release();
+                    }
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+
+        Thread thread3 = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    for (int i = 0; i < 10; i++) {
+                        Z.acquire();
+                        System.out.println("Z");
+                        X.release();
+                    }
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+
+        thread1.start();
+        thread2.start();
+        thread3.start();
     }
 }
